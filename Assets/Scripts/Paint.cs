0136af3112239db35cb6e61902fec11a33f964f4
KarlaCarreto <a01652799@itesm.mx>
2019-05-05 02:20:16 -0500

    ColorMatch

---
+++ Assets/Scripts/Paint.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Paint: MonoBehaviour
+{
+    public float upForce = 200f;
+    public float score = 0f;
+    public Text scText;
+    private bool isDead = false;
+    public GameObject gameOver;
+    private Rigidbody2D rb2d;               
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (isDead == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isDead = true;
+        gameOver.SetActive(true);
+        
+       
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Bad" || col.gameObject.tag == "Floor")
+        {
+            Debug.Log("NOOOOOOOOOO");
+            isDead = true;
+            gameOver.SetActive(true);
+
+        }
+        else
+        {
+            Debug.Log("It's correct");
+            score++;
+            scText.text = "Score: " + score;
+        }
+    }
+}
---
+++ Assets/Scripts/matriz.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class matriz : MonoBehaviour
+
+{
+    public float maxTi = 1;
+    private float timer = 0;
+    public float height;
+    public GameObject shark;
+    public GameObject[] myArray;    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var element = myArray[Random.Range(0, myArray.Length)];
+        if (timer > maxTi)
+        {
+            GameObject newShark = Instantiate(element);
+            newShark.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newShark, 15);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+
+    }
+
+
+    
+
+
+ 
+}
